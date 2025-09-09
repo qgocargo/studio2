0fa55aa173b5c9ee4979e6a798ae2db5ec594fc2
akif bade <kiffbade@gmail.com>
2025-09-09 13:22:26 +0000

    GIVE ME STEPS HOW TO DIPLOY ON VERCEL

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,10 +1,10 @@
 // It's recommended to store these in your environment variables
 // For example, in a .env.local file with NEXT_PUBLIC_ prefix
 export const firebaseConfig = {
-  projectId: "fieldforce-manager-3wsaz",
-  appId: "1:315321660144:web:7bf2f0afd43244c246c835",
-  storageBucket: "fieldforce-manager-3wsaz.appspot.com",
-  apiKey: "AIzaSyBEyO2yXngcoOc7ZZBOaCyuzjC--LWC1LM",
-  authDomain: "fieldforce-manager-3wsaz.firebaseapp.com",
-  messagingSenderId: "315321660144",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
