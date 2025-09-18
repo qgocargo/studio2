24617412495dfe2b34ef6083c8dfc652bd73f07d
akif bade <kiffbade@gmail.com>
2025-09-18 19:58:40 +0000

    // --- Firebase Configuration ---         const firebaseConfig = {

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './public/api/index.html'
+    './api/DATA.HTML'
   ],
   theme: {
     extend: {
