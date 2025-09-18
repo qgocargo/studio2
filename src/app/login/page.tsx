f1d4827c53ffabe10d72e0fb6ae0ae72427ffc06
akif bade <kiffbade@gmail.com>
2025-09-18 20:44:54 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -56,6 +56,7 @@ export default function LoginPage() {
                         required
                         className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                         placeholder="Full Name"
+                        suppressHydrationWarning
                         />
                     </div>
                 )}
@@ -69,6 +70,7 @@ export default function LoginPage() {
                     required
                     className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 ${isLogin ? 'rounded-t-md' : ''} focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm`}
                     placeholder="Email address"
+                    suppressHydrationWarning
                     />
                 </div>
                 <div>
@@ -81,6 +83,7 @@ export default function LoginPage() {
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Password"
+                    suppressHydrationWarning
                     />
                 </div>
             </div>
