659ea524efe7d3ffb5980da2105345f24d6344a4
akif bade <kiffbade@gmail.com>
2025-09-18 20:20:09 +0000

    vercel ab aisa dik raha hai

--- next.config.ts
+++ next.config.ts
@@ -24,14 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ]
-  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import './iframe.css';
 
 export default function HomePage() {
   return (
-    <iframe src="/api/DATA.HTML" title="HTML Content" />
+    <iframe src="/DATA.HTML" title="HTML Content" />
   );
 }
